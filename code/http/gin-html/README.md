525621f3de84ccafae8802a8ffdc5f08f79b9887
Seungmin Lee <lsmin0703@telcoware.com>
2022-10-04 16:56:53 +0900

    gin-html 연습 :bulb:

---
+++ code/http/gin-html/README.md
@@ -0,0 +1,8 @@
+# gin-html
+기본적으로 gin router를 통해서 html page를 server side에서 rendering 하는 방법에 대해서 설명했다.
+
+# 문제점 
+templates Directory에 여러 html 파일들을 올려두지만 , Page가 많아지면 찾아보기 어려운점이 있다. 
+templates Directory이 밑에 Sub Directory를 구성하여 관리 할 수 있는 방법에 대해서 확인해보자
+> 사람들이 issues 모음집
+>> https://github.com/gin-gonic/gin/issues/78
---
+++ code/http/gin-html/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"gin-html/data"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Static 파일 Read 여기에 css, js, image 등이 올라간다.
+	r.Static("/static", "./static/")
+
+	// html templates 모음집이다.
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{"title": "HOME"})
+		return
+	})
+
+	r.GET("/user", func(c *gin.Context) {
+		users, err := data.User().GetUserList()
+		if err != nil {
+			c.HTML(http.StatusNotFound, "error.html", gin.H{"title": "정보 조회 실패", "error": ""})
+		} else {
+			c.HTML(http.StatusOK, "users.html", gin.H{"title": "유저 정보 조회", "users": users})
+		}
+		return
+	})
+
+	r.GET("/user/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		user, err := data.User().GetUser(id)
+		if err != nil {
+			c.HTML(http.StatusNotFound, "error.html", gin.H{"title": id + "정보 조회 실패", "error": ""})
+		} else {
+			c.HTML(http.StatusOK, "user.html", gin.H{"title": id + "정보 조회", "user": user})
+		}
+		return
+	})
+
+	r.Run()
+}
